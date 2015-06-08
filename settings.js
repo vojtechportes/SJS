dc785b2d3c1d5b61086bc237fa35d44a3c845f06
Vojtěch Václav Porteš <info@justart.org>
2015-06-08 10:52:49 +0200

    Readme update, settings update

--- settings.js
+++ settings.js
@@ -1,8 +1,7 @@
 var settings = {
 	"browserSupport": {
-		"s.modern": ["ie10", "chrome", "firefox"],
-		"s": ["ie", "ie10", "firefox", "chrome"],
-		"s.chrome": ["chrome"]
+		"s.modern": [],
+		"s": ["ie"],
 	},
 	"modules": ["Core", "Array", "Type", "String", "Each", "Element", "Element.Class", "Element.Style", "Element.Event", "Request"]
 }; 
